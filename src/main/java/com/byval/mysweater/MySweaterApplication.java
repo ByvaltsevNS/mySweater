6a80458cd502ffd133bd6476a268f1c6ebe7f436
byval <byvaltsev1978@gmail.com>
2022-03-17 14:18:50 +0300

    Initial commit

---
+++ src/main/java/com/byval/mysweater/MySweaterApplication.java
@@ -0,0 +1,11 @@
+package com.byval.mysweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySweaterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySweaterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/byval/mysweater/MySweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byval.mysweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySweaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
